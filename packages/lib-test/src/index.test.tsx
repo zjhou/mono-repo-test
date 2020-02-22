004f522140ebf50cff60ac84054bcd9396b1cf03
ZjHOU <z@zjh.im>
2020-02-22 10:33:00 +0800

    test

--- packages/lib-test/src/index.test.tsx
+++ packages/lib-test/src/index.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Lib from './index.tsx';
 
+// hello
 test('renders learn react link', () => {
   const { getByText } = render(<Lib />);
   const linkElement = getByText(/HELLO/i);
